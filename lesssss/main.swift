3c1c2e736caa99aa195e3064b816be316a193086
RuslanAlidzhanov <rusik749@gmail.com>
2023-09-23 15:54:01 +0300

    calculate percent

--- lesssss/main.swift
+++ lesssss/main.swift
@@ -6,6 +6,11 @@
 //
 
 import Foundation
+// Домащнее задание 1. Расчет процента по вкладу
+var sumV = 1000
+var percent = 5
+var years = 5
 
-print("Hello, World!")
+var itogo = (sumV*percent*5)/100
+print("Начисленные проценты за \(years) лет : \(itogo) руб.")
 
